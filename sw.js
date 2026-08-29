b76921fac90e11ea8674cf193cf6e0204addadaf
andresmila0715 <andresmila0715@gmail.com>
2026-08-29 16:21:16 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ebs-app-v41';
+const CACHE_NAME = 'ebs-app-v43';
 
 // Archivos precacheados al instalar — config.js incluido para login offline
 const ASSETS = [
@@ -83,3 +83,4 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
